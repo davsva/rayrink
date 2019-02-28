a7c661a70d632d9d8c6daae640d595aaef51b556
David Svanberg <hacker4@OTAPRD537ops.onmicrosoft.com>
2019-02-28 21:36:38 +0100

    Add project files.

---
+++ rayrink/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rayrink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
